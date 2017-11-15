d31a2a177b392f2dbf360ffea10c78ab0b46e723
Andrew Shopland <hi@ashp.land>
2017-11-15 15:38:22 -0800

    Added BoardView

---
+++ Whiteboard/BoardView.swift
@@ -0,0 +1,26 @@
+//
+//  BoardView.swift
+//  Whiteboard
+//
+//  Created by Andrew on 2017-11-15.
+//  Copyright © 2017 hearthedge. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+
+}
