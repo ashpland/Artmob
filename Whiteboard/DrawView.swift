7993f5d01270c709bfbbfb14729fba0c420fce7d
Andrew Shopland <hi@ashp.land>
2017-11-15 17:24:05 -0800

    drawing works 🎨

--- Whiteboard/DrawView.swift
+++ Whiteboard/DrawView.swift
@@ -22,12 +22,38 @@ class DrawView: UIView {
             let nextLineSegment = LineSegment(firstPoint: first, secondPoint: second)
             activeDrawingLine.addSegment(nextLineSegment)
         }
+        
+        self.setNeedsDisplay()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.lineDelegate.newLine(activeDrawingLine)
         activeDrawingLine = Line()
+        self.setNeedsDisplay()
     }
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+        path.lineWidth = 5.0
+        path.lineCapStyle = CGLineCap.round
+        UIColor.blue.setStroke()
+        
+        if !activeDrawingLine.segments.isEmpty {
+            path.move(to: activeDrawingLine.segments.first!.firstPoint)
+            
+            for segment in activeDrawingLine.segments {
+                path.addLine(to: segment.firstPoint)
+                path.addLine(to: segment.secondPoint)
+            }
+            path.stroke()
+        }
+        
+        
+        
+        
+    }
+    
+    
 }
 
 
