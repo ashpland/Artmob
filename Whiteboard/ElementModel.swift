feb4c4186ae7b72e287702b29edf7c4f490e07de
Andrew Shopland <hi@ashp.land>
2017-11-17 18:10:27 -0800

    Cleaned up ElementModel, removed storage of LineElements, removed references to ViewModel

--- Whiteboard/ElementModel.swift
+++ Whiteboard/ElementModel.swift
@@ -14,10 +14,6 @@ class ElementModel {
     
     static let sharedInstance = ElementModel()
     
-    // TODO: remove this later probably
-    public var viewModel : BoardViewModel?
-    
-    private var lines = [LineElement]()
     let lineSubject = PublishSubject<[LineElement]>()
     
     private var labels = [Stamp : LabelElement]()
@@ -26,17 +22,9 @@ class ElementModel {
         switch instruction.element {
         case .line (let lineToDraw):
             self.lineSubject.onNext([lineToDraw])
-            self.addLine(lineToDraw)
         case .emoji:
             self.processLabel(instruction)
         }
-    }
-    private func addLine(_ newLine: LineElement) {
-        
-        
-        self.lines.append(newLine)
-        // TODO: remove drawLines call and have LineElements sent by sequence
-//        self.viewModel?.drawLines([newLine])
     }
     
     private func processLabel(_ labelInstruction: Instruction) {
