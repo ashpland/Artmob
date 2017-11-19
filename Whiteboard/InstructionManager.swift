6401163a540539f057ce6529bf68cfda9cae3841
Andrew Shopland <hi@ashp.land>
2017-11-18 22:56:32 -0800

    code cleanup

--- Whiteboard/InstructionManager.swift
+++ Whiteboard/InstructionManager.swift
@@ -10,26 +10,25 @@ import Foundation
 import RxSwift
 
 class InstructionManager {
-    
+
     static let sharedInstance = InstructionManager()
-    
+
     private var instructionStore = [Instruction]()
     let newInstructions = PublishSubject<Instruction>()
     let broadcastInstructions = PublishSubject<Instruction>()
     private let disposeBag = DisposeBag()
-    
-    
+
     // MARK: - Methods
-    
+
     class func subscribeToInstructionsFrom(_ newPublishSubject: PublishSubject<Instruction>) {
         newPublishSubject.subscribe(onNext: { instruction in
             InstructionManager.sharedInstance.newInstruction(instruction)
         }).disposed(by: InstructionManager.sharedInstance.disposeBag)
     }
-    
+
     private func newInstruction(_ newInstruction: Instruction) {
-        if (self.instructionStore.isEmpty ||
-            newInstruction.stamp > self.instructionStore.last!.stamp) {
+        if self.instructionStore.isEmpty ||
+            newInstruction.stamp > self.instructionStore.last!.stamp {
             self.instructionStore.append(newInstruction)
             self.newInstructions.onNext(newInstruction)
             self.broadcastInstructions.onNext(newInstruction)
@@ -44,32 +43,29 @@ class InstructionManager {
                     self.broadcastInstructions.onNext(newInstruction)
 
                     switch newInstruction.element {
-                    case .line(_):
+                    case .line:
                         self.refreshLines()
                         return
-                    case .emoji(_):
+                    case .emoji:
                         return
                     }
                 }
             }
         }
     }
-    
+
     private func refreshLines() {
-        let lineInstructions = self.instructionStore.filter{ if case .line = $0.element { return true }; return false}
+        let lineInstructions = self.instructionStore.filter { if case .line = $0.element { return true }; return false}
         ElementModel.sharedInstance.refreshLines(from: lineInstructions)
     }
-    
-    
 }
-
 
 // MARK: - Instruction components
 
 struct Instruction {
-    let type : InstructionType
-    let element : InstructionPayload
-    let stamp : Stamp
+    let type: InstructionType
+    let element: InstructionPayload
+    let stamp: Stamp
 }
 
 enum InstructionType {
@@ -81,45 +77,36 @@ enum InstructionType {
 enum InstructionPayload {
     case line (LineElement)
     case emoji (LabelElement)
-    
+
     var lineElement: LineElement? {
         guard case .line(let value) = self else {
             return nil
         }
         return value
     }
-    
-    
-    
 }
 
 struct Stamp: Comparable, Hashable {
     var hashValue: Int {
-        get {
             let timeHash = self.timestamp.hashValue
             let userHash = self.user.hashValue
             return timeHash ^ userHash &* 16777619
-        }
     }
-    
-    static func <(lhs: Stamp, rhs: Stamp) -> Bool {
-        if (lhs.timestamp < rhs.timestamp) {
+
+    static func < (lhs: Stamp, rhs: Stamp) -> Bool {
+        if lhs.timestamp < rhs.timestamp {
             return true
         }
-        if (lhs.timestamp == rhs.timestamp && lhs.user < rhs.user) {
+        if lhs.timestamp == rhs.timestamp && lhs.user < rhs.user {
             return true
         }
         return false
     }
-    
-    static func ==(lhs: Stamp, rhs: Stamp) -> Bool {
+
+    static func == (lhs: Stamp, rhs: Stamp) -> Bool {
         return ((lhs.user == rhs.user) && (lhs.timestamp == rhs.timestamp))
     }
-    
-    
-    
-    let user : String
-    let timestamp : Date
-    
-    
+
+    let user: String
+    let timestamp: Date
 }
