403c1b4eaa7c7eac9d2d0b189421c71d07930fc1
Aaron Johnson <aaron9johnson@gmail.com>
2017-11-23 02:04:39 -0800

    test

--- Whiteboard/BoardViewController.swift
+++ Whiteboard/BoardViewController.swift
@@ -10,56 +10,96 @@ import UIKit
 import RxSwift
 import MultipeerConnectivity
 
-class BoardViewController: UIViewController, MCBrowserViewControllerDelegate, CloseMenu, UITextFieldDelegate {
-    func closeMenu() {
-        emojiTextField.endEditing(false)
-        UIView.animate(withDuration: 0.4, animations: {
-            self.MainMenuHeight.constant = -160
-            self.MainMenuButton.transform = CGAffineTransform(rotationAngle: 0)
-            self.view.layoutIfNeeded()
-        })
-        
-    }
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        textSelected = true
-    }
-    var textSelected:Bool!
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        UIView.animate(withDuration: 0.25, animations: {
-            self.MainMenuHeight.constant = 0
-            self.MainMenuButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-            self.view.layoutIfNeeded()
-        })
-        
-        textField.resignFirstResponder()
-        return true
-    }
-    @IBOutlet weak var emojiTextField: UITextField!
+class LineFormatSettings {
+    static let sharedInstance = LineFormatSettings()
     
-    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-        mpcHandler.browser.dismiss(animated: true, completion: nil)
-    }
-    
-    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-        mpcHandler.browser.dismiss(animated: true, completion: nil)
-    }
+    var width : CGFloat = 5.0
+    //var cap = CGLineCap.round
+    var cap = CGLineCap.square
+    var color = LineColor.blue
+}
 
+class BoardViewController: UIViewController, MCBrowserViewControllerDelegate, CloseMenu, UITextFieldDelegate {
+    
+    //MARK: Properties / Outlets
+    let viewModel = BoardViewModel()
+    let disposeBag = DisposeBag()
+    var mpcHandler = MPCHandler.sharedInstance
+    var textSelected:Bool!
+    
+    @IBOutlet weak var emojiTextField: UITextField!
     @IBOutlet weak var MainMenuButton: UIButton!
     @IBOutlet weak var MainMenuHeight: NSLayoutConstraint!
+    @IBOutlet weak var drawView: DrawView!
+    @IBOutlet weak var lineImageView: UIImageView!
+    @IBOutlet var ColorButtons: [UIButton]!
+    
+    //MARK: Load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateColorButtons()
+        setUpMPC()
+        setUpModel()
+        setUpMenu()
+    }
+    
+    //MARK:Setup
+    func updateColorButtons(){
+        for button in ColorButtons{
+            button.backgroundColor = LineElement(line: Line(), width: 69.69, cap: .butt, color: LineColor(rawValue: button.tag)!).drawColor //sooo janky
+        }
+    }
+    func setUpMPC(){
+        mpcHandler.setupPeerWithDisplayName(displayName: UIDevice.current.name)
+        mpcHandler.setupSession()
+        mpcHandler.advertiseSelf(advertise: true)
+        mpcHandler.setupSubscribe()
+    }
+    func setUpModel(){
+        drawView.closeMenuDelagate = self
+        drawView.clearsContextBeforeDrawing = true
+        drawView.viewModel = self.viewModel
+        
+        self.viewModel.lineImage.asObservable()
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { lineImage in
+                UIView.transition(with: self.view,
+                                  duration: 0.25,
+                                  options: UIViewAnimationOptions.transitionCrossDissolve,
+                                  animations: { self.lineImageView.image = lineImage },
+                                  completion: nil)
+            }).disposed(by: disposeBag)
+    }
+    func setUpMenu(){
+        MainMenuHeight.constant = -160
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: NSNotification.Name.UIKeyboardWillShow,
+            object: nil
+        )
+    }
+    
+    //MARK: Actions
+    
     @IBAction func thickness(_ sender: UIButton) {
         let formatLine = LineFormatSettings.sharedInstance
         switch sender.tag{
         case 0:
             formatLine.width = 3.0
+            MainMenuButton.titleLabel?.font = MainMenuButton.titleLabel?.font.withSize(24.0)
             break
         case 1:
             formatLine.width = 5.0
+            MainMenuButton.titleLabel?.font = MainMenuButton.titleLabel?.font.withSize(40.0)
             break
         default:
-            formatLine.width = 8.0
+            formatLine.width = 11.0
+            MainMenuButton.titleLabel?.font = MainMenuButton.titleLabel?.font.withSize(56.0)
             break
         }
     }
+    
     @IBAction func color(_ sender: UIButton) {
         print(sender.tag)
         textSelected = false
@@ -84,15 +124,16 @@ class BoardViewController: UIViewController, MCBrowserViewControllerDelegate, Cl
         default:
             formatLine.color = LineColor.black
         }
-        MainMenuButton.setTitleColor(LineElement(line: Line(), width: 0, cap: .butt, color: formatLine.color).drawColor, for: UIControlState.normal)
+        MainMenuButton.setTitleColor(LineElement(line: Line(), width: 0, cap: .butt, color: formatLine.color).drawColor, for: UIControlState.normal) //jankness
     }
-
-    @IBOutlet weak var drawView: DrawView!
-    @IBOutlet weak var lineImageView: UIImageView!
     
-    let viewModel = BoardViewModel()
-    let disposeBag = DisposeBag()
-    var mpcHandler = MPCHandler.sharedInstance
+    @IBAction func Add(_ sender: Any) {
+        if mpcHandler.session != nil{
+            mpcHandler.setupBrowser()
+            mpcHandler.browser.delegate = self
+            self.present(mpcHandler.browser, animated: true, completion: nil)
+        }
+    }
     
     @IBAction func Menu(_ sender: UIButton) {
         
@@ -103,28 +144,48 @@ class BoardViewController: UIViewController, MCBrowserViewControllerDelegate, Cl
                 self.view.layoutIfNeeded()
             })
         } else if MainMenuHeight.constant == 0{
-            UIView.animate(withDuration: 0.5, animations: {
-                self.MainMenuHeight.constant = -160
-                self.MainMenuButton.transform = CGAffineTransform(rotationAngle: 0)
-                self.view.layoutIfNeeded()
-            })
-        } else {
+            closeMenu()
+        } else { //Keyboard Open
             emojiTextField.endEditing(false)
-            UIView.animate(withDuration: 0.35, animations: {
-                self.MainMenuHeight.constant = -160
-                self.MainMenuButton.transform = CGAffineTransform(rotationAngle: 0)
-                self.view.layoutIfNeeded()
-            })
-        }
-        
-    }
-    @IBAction func Add(_ sender: Any) {
-        if mpcHandler.session != nil{
-            mpcHandler.setupBrowser()
-            mpcHandler.browser.delegate = self
-            self.present(mpcHandler.browser, animated: true, completion: nil)
+            closeMenu()
         }
     }
+    
+    //MARK: Close Menu Delegate
+    func closeMenu() {
+        emojiTextField.endEditing(false)
+        UIView.animate(withDuration: 0.4, animations: {
+            self.MainMenuHeight.constant = -160
+            self.MainMenuButton.transform = CGAffineTransform(rotationAngle: 0)
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    //MARK: TextField Delegate
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textSelected = true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.MainMenuHeight.constant = 0
+            self.MainMenuButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            self.view.layoutIfNeeded()
+        })
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: Browser View Controller Delegate
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        mpcHandler.browser.dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        mpcHandler.browser.dismiss(animated: true, completion: nil)
+    }
+
+    //MARK: Keyboard Will Show
     @objc func keyboardWillShow(_ notification: Notification) {
         if let keyboardFrame: NSValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
             let keyboardRectangle = keyboardFrame.cgRectValue
@@ -134,60 +195,6 @@ class BoardViewController: UIViewController, MCBrowserViewControllerDelegate, Cl
                 self.MainMenuButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
                 self.view.layoutIfNeeded()
             })
-            
         }
     }
-    @IBOutlet var ColorButtons: [UIButton]!
-    
-    func setUpColorButtons(){
-        for button in ColorButtons{
-            button.backgroundColor = LineElement(line: Line(), width: 0, cap: .butt, color: LineColor(rawValue: button.tag)!).drawColor //sooo janky
-        }
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setUpColorButtons()
-        
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(keyboardWillShow),
-            name: NSNotification.Name.UIKeyboardWillShow,
-            object: nil
-        )
-        
-        MainMenuHeight.constant = -160
-        
-        mpcHandler.setupPeerWithDisplayName(displayName: UIDevice.current.name)
-        mpcHandler.setupSession()
-        mpcHandler.advertiseSelf(advertise: true)
-        mpcHandler.setupSubscribe()
-        
-        
-        
-        drawView.closeMenuDelagate = self
-        drawView.clearsContextBeforeDrawing = true
-        drawView.viewModel = self.viewModel
-        
-        self.viewModel.lineImage.asObservable()
-            .observeOn(MainScheduler.instance)
-            .subscribe(onNext: { lineImage in
-                UIView.transition(with: self.view,
-                                  duration: 0.25,
-                                  options: UIViewAnimationOptions.transitionCrossDissolve,
-                                  animations: { self.lineImageView.image = lineImage },
-                                  completion: nil)
-            })
-            .disposed(by: disposeBag)
-    }
 }
-
-class LineFormatSettings {
-    static let sharedInstance = LineFormatSettings()
-    
-    var width : CGFloat = 5.0
-    var cap = CGLineCap.round
-    var color = LineColor.blue
-}
-
-
-
